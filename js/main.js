f22c252f6a31c80474a60f8cd4bc5c462cff5a03
sacco815 <43184757+sacco815@users.noreply.github.com>
2022-11-09 19:11:37 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * *
+*           MAIN           *
+* * * * * * * * * * * * * */
+
+// init global variables & switches
+
+// load data using promises
+let promises = [
+    d3.csv("data/BeatlesAlbumCoverPalettes.csv")
+];
+
+Promise.all(promises)
+    .then(function (data) {
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    // log data
+    console.log('check out the data', dataArray);
+
+    // init visualizations
+
+}
+
+
