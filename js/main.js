6440defe5e9d40146ec2437133acb3411d2cb7c6
sacco815 <michaelsacco@college.harvard.edu>
2022-11-13 18:44:44 -0500

    VisOne added

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * *
+*           MAIN           *
+* * * * * * * * * * * * * */
+
+// init global variables & switches
+let albumVis;
+
+// load data using promises
+let promises = [
+    d3.csv("data/BeatlesAlbumCoverPalettes.csv")
+];
+
+Promise.all(promises)
+    .then(function (data) {
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    // log data
+    console.log('check out the data', dataArray);
+
+    // init visualizations
+    albumVis = new AlbumVis('visOne', dataArray[0]);
+
+}
+
+
